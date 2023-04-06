f742a6e6569c73bf14c0afb45b3480fcacc7ae7b
RWindesheim <104098243+RWindesheim@users.noreply.github.com>
2023-04-06 15:40:55 +0200

    Update rlhf.py

--- src/reward_modelling/rlhf.py
+++ src/reward_modelling/rlhf.py
@@ -1,17 +1,3 @@
-# coding=utf-8
-# Copyright 2022 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from dataclasses import dataclass, field
 from typing import Optional 
 
@@ -30,37 +16,14 @@ from preference_datasets import AnthropicHelpfulHarmless
 
 tqdm.pandas()
 
-########################################################################
-# This is a fully working simple example to use trl with accelerate.
-#
-# This example fine-tunes a GPT2 model on the IMDB dataset using PPO
-# (proximal policy optimization).
-# in any of the following settings (with the same script):
-#   - single CPU or single GPU
-#   - multi GPUS (using PyTorch distributed mode)
-#   - multi GPUS (using DeepSpeed ZeRO-Offload stages 1 & 2)
-#   - fp16 (mixed-precision) or fp32 (normal precision)
-#
-# To run it in each of these various modes, first initialize the accelerate
-# configuration with `accelerate config`
-#
-########################################################################
 
-
-# We first define the configuration of the experiment, defining the model, the dataset,
-# the training parameters, and the PPO parameters.
-# Check the default arguments in the `PPOConfig` class for more details.
-# If you want to log with tensorboard, add the kwarg
-# `accelerator_kwargs={"logging_dir": PATH_TO_LOGS}` to the PPOConfig.
-# Define and parse arguments.
 @dataclass
 class ScriptArguments:
     """
     The name of the Casual LM model we wish to fine with PPO
     """
 
-    # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
-    # models like gpt-neo* models are more suitable.
+  
     model_name: Optional[str] = field(default="lvwerra/gpt2-imdb", metadata={"help": "the model name"})
     log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
     learning_rate: Optional[float] = field(default=1.41e-5, metadata={"help": "the learning rate"})
@@ -88,42 +51,9 @@ config = PPOConfig(
 )
 
 
-# We then define the arguments to pass to the sentiment analysis pipeline.
-# We set `return_all_scores` to True to get the sentiment score for each token.
+
 sent_kwargs = {"return_all_scores": True, "function_to_apply": "none", "batch_size": 16}
 
-
-# Below is an example function to build the dataset. In our case, we use the IMDB dataset
-# from the `datasets` library. One should customize this function to train the model on
-# its own dataset.
-# def build_dataset(config, dataset_name="imdb", input_min_text_length=2, input_max_text_length=8):
-#      """
-#     Build dataset for training. This builds the dataset from `load_dataset`, one should
-#     customize this function to train the model on its own dataset.
-#     Args:
-#         dataset_name (`str`):
-#             The name of the dataset to be loaded.
-#     Returns:
-#         dataloader (`torch.utils.data.DataLoader`):
-#             The dataloader for the dataset.
-#     """
-#     tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-#     tokenizer.pad_token = tokenizer.eos_token
-#     # load imdb with datasets
-#     ds = load_dataset(dataset_name, split="train")
-#     ds = ds.rename_columns({"text": "review"})
-#     ds = ds.filter(lambda x: len(x["review"]) > 200, batched=False)
-
-#     input_size = LengthSampler(input_min_text_length, input_max_text_length)
-
-#     def tokenize(sample):
-#         sample["input_ids"] = tokenizer.encode(sample["review"])[: input_size()]
-#         sample["query"] = tokenizer.decode(sample["input_ids"])
-#         return sample
-
-#     ds = ds.map(tokenize, batched=False)
-# ds.set_format(type="torch")
-#     return ds
 
 
 def build_dataset(dataset_name, data_dir=None, split='train'):
@@ -153,42 +83,27 @@ class LMCollator:
         for k,v in outputs.items():
             output_dict[k] = [v_ for v_ in v]
         return output_dict
-        #return {'input_ids': [t for t in outputs]}
-# We retrieve the dataloader by calling the `build_dataset` function.
+   
 dataset = build_dataset("Anthropic/hh-rlhf", data_dir='harmless-base', split='train')
 
-
-# def collator(data):
-#     return dict((key, [d[key] for d in data]) for key in data[0])
-
-
-
-# set seed before initializing value head for deterministic eval
 set_seed(config.seed)
 
-# Now let's build the model, the reference model, and the tokenizer.
+
 model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
 ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
 tokenizer = AutoTokenizer.from_pretrained(config.model_name, padding_side='left')
 
-# GPT-2 tokenizer has a pad token, but it is not eos_token by default. We need to set it to eos_token.
-# only for this model.
 tokenizer.pad_token = tokenizer.eos_token
 
-# We then build the PPOTrainer, passing the model, the reference model, the tokenizer
 ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, 
     data_collator=LMCollator(tokenizer))
 
-# We then build the sentiment analysis pipeline, passing the model name and the
-# sentiment analysis pipeline arguments. Let's also make sure to set the device
-# to the same device as the PPOTrainer.
 device = ppo_trainer.accelerator.device
 if ppo_trainer.accelerator.num_processes == 1:
-    device = 0 if torch.cuda.is_available() else "cpu"  # to avoid a `pipeline` bug
-# sentiment_pipe = pipeline("sentiment-analysis", model="lvwerra/distilbert-imdb", device=device)
-# rm_path = '/juice2/scr2/fongsu/reward_model_HH/2023-03-31 16:24:49.186242/pytorch_model.bin'
+    device = 0 if torch.cuda.is_available() else "cpu"  
+
 rm_path = '/juice2/scr2/fongsu/reward_model_HH/2023-04-04 23:27:29.175109/pytorch_model.bin'
-# reward_model_name_or_path = 'EleutherAI/gpt-neo-1.3B'
+
 reward_model_name_or_path = 'distilbert-base-uncased'
 reward_model = RewardModel(reward_model_name_or_path)
 reward_model.load_state_dict(torch.load(rm_path))
@@ -199,9 +114,6 @@ rm_tokenizer = AutoTokenizer.from_pretrained(reward_model_name_or_path, max_leng
 if rm_tokenizer.pad_token == None:
     rm_tokenizer.pad_token = tokenizer.eos_token
 
-# We then define the arguments to pass to the `generate` function. These arguments
-# are passed to the `generate` function of the PPOTrainer, which is a wrapper around
-# the `generate` function of the trained model.
 generation_kwargs = {
     "min_length": -1,
     "top_k": 0.0,
@@ -213,24 +125,30 @@ output_min_length = 4
 output_max_length = 16
 output_length_sampler = LengthSampler(output_min_length, output_max_length)
 
+
 for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
     query_tensors = batch["input_ids"]
 
-    # Get response from gpt2
+    # Adding the weighing
+    prompts = tokenizer.batch_decode(query_tensors, skip_special_tokens=True)
+    w1_list = [random.uniform(0, 1) for _ in range(len(prompts))]
+    w2_list = [1 - w1 for w1 in w1_list]
+    prompts = [f"{prompt} [W1={w1:.2f}][W2={w2:.2f}]" for prompt, w1, w2 in zip(prompts, w1_list, w2_list)]
+    query_tensors = tokenizer.batch_encode_plus(prompts, padding=True, max_length=512, return_tensors="pt")["input_ids"]
+
     response_tensors = ppo_trainer.generate(
         query_tensors, return_prompt=False, length_sampler=output_length_sampler, **generation_kwargs
     )
     batch["response"] = tokenizer.batch_decode(response_tensors)
 
-    # Compute reward from reward model
-    outputs = reward_model(**rm_tokenizer(batch['response'], return_tensors='pt', padding=True, truncation=True).to(device))
-    rewards = [torch.tensor(output) for output in outputs.tolist()]
+    outputs1 = reward_model(**rm_tokenizer(batch['response'], return_tensors='pt', padding=True, truncation=True).to(device))
+    rewards1 = [torch.tensor(output) for output in outputs1.tolist()]
 
-    # Compute sentiment score
-    # texts = [q + r for q, r in zip(batch["query"], batch["response"])]
-    # pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
-    # rewards = [torch.tensor(output[1]["score"]) for output in pipe_outputs]
+    outputs2 = reward_model2(**rm_tokenizer2(batch['response'], return_tensors='pt', padding=True, truncation=True).to(device))
+    rewards2 = [torch.tensor(output) for output in outputs2.tolist()]
 
-    # Run PPO step
+    # Combine weighting + reward
+    rewards = [w1 * r1 + w2 * r2 for w1, w2, r1, r2 in zip(w1_list, w2_list, rewards1, rewards2)]
+
     stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
     ppo_trainer.log_stats(stats, batch, rewards)
